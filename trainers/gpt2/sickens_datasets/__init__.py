036bfafc2adf1ee89527a0c8b94dda4dbd0434cc
Adrian Chojnicki <adrian@chojnicki.info>
2023-11-05 15:29:30 +0000

    changes

--- trainers/gpt2/sickens_datasets/__init__.py
+++ trainers/gpt2/sickens_datasets/__init__.py
@@ -3,7 +3,7 @@ import sickens_datasets.oxford_dictionary
 import sickens_datasets.simple_wiki
 
 datasets={
-	"Sicken"            : sickens_datasets.sicken.Sicken,
-	"Oxford_Dictionary" : sickens_datasets.oxford_dictionary.Oxford_Dictionary,
-	"Simple_Wiki"       : sickens_datasets.simple_wiki.Simple_Wiki
+	"sicken"            : sickens_datasets.sicken.Sicken,
+	"oxford_dictionary" : sickens_datasets.oxford_dictionary.Oxford_Dictionary,
+	"simple_wiki"       : sickens_datasets.simple_wiki.Simple_Wiki
 	}
