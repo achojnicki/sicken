9bfc607f0f1ba81ecbbc9143116b8dd1aa96ba5f
Adrian Chojnicki <adrian@chojnicki.dev>
2025-03-08 22:40:13 +0000

    changes in the MacOS installer

--- install/install_macos.sh
+++ install/install_macos.sh
@@ -51,7 +51,7 @@ run "brew install python@3.12"
 
 whereis pip3
 print "2nd stage installation of dependencies"
-run "/usr/local/bin/python3.12 -m pip install --break-system-packages numpy openai playsound flask flask-socketio python-socketio psutil tabulate colored pymongo pyyaml pika uwsgi websockets"
+run "/usr/local/bin/python3.12 -m pip install --break-system-packages numpy openai playsound flask flask-socketio python-socketio psutil tabulate colored pymongo pyyaml pika uwsgi websockets pyobjc"
 
 
 print "Installing MongoDB database"
